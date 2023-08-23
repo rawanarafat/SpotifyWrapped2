b43d03d304e5a9c30920af3abdf44756998a4afa
rawanarafat <rawanarafat@hotmail.com>
2023-08-23 19:11:25 -0400

    changes to login

--- server/src/routes/login.js
+++ server/src/routes/login.js
@@ -4,28 +4,26 @@ import axios from "axios";
 
 const router = express.Router();
 
+// Update your login route to use the deployment URL
 router.get("/login", async (req, res) => {
   try {
-    // Your existing code to construct the authorizationURL
     const queryString = querystring.stringify({
       response_type: "code",
       client_id: "5bb070ff4bdd46cf8598411dc53e7b8a",
-      redirect_uri: "http://localhost:7001/auth/callback",
+      // Update the redirect_uri to match your deployment URL
+      redirect_uri: "https://spotifywrapped.onrender.com/auth/callback",
       scope: "user-library-read, user-top-read",
     });
 
     const authorizationURL =
       "https://accounts.spotify.com/authorize?" + queryString;
 
-    // Redirect the user to the Spotify authorization URL
-    //res.redirect(authorizationURL);
-
     res.status(200).json({ authorizationURL });
   } catch (error) {
-    // Handle any errors that occurred during the process
     res.status(500).send("An error occurred");
   }
 });
+
 
 router.get("/callback", async function (req, res) {
   const code = req.query.code || null;
@@ -36,7 +34,8 @@ router.get("/callback", async function (req, res) {
       method: "post",
       params: {
         code: code,
-        redirect_uri: "http://localhost:7001/auth/callback",
+        // Update the redirect_uri to match your deployment URL
+        redirect_uri: "https://spotifywrapped.onrender.com/auth/callback",
         grant_type: "authorization_code",
       },
       headers: {
@@ -50,24 +49,16 @@ router.get("/callback", async function (req, res) {
 
     const response = await axios(authOptions);
 
-    // You can use the access token from the response to make requests to Spotify's API on behalf of the user
-    const access_token = response.data.access_token;
-    // You might also receive a refresh token, depending on the scope you requested
-    res.cookie("access_token", access_token);
-    res.cookie("spotifyExpiry", response.data.expires_in);
-    res.cookie("spotifyRefreshToken", response.data.refresh_token);
-    const tokenCreationTime = Date.now();
-    res.cookie("tokenCreationTime", tokenCreationTime);
-    // Further logic and processing can be performed here, such as saving the access token in a database or using it to fetch user data from Spotify
-    console.log(response.data, " + ", access_token);
-    //res.send("Authorization successful!");
-    //res.status(200).send("Authorization successful!");
-    res.redirect("http://localhost:2002/mytop/topTracks");
-    // Respond to the client or redirect to another page after successful authorization
+    // Your existing logic
+    // ...
+
+    // Update the redirect URL after successful authorization
+    res.redirect("https://spotifywrapped.onrender.com/mytop/topTracks");
   } catch (error) {
     console.error("Error exchanging the authorization code:", error);
     res.status(500).send("An error occurred during authorization.");
   }
 });
+
 
 export { router as userRouter };
