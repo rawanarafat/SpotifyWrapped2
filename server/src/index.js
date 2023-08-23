887f6b0bbd13cb3f12f2dbf9abe0c4c6d916806e
rawanarafat <rawanarafat@hotmail.com>
2023-08-23 14:14:23 -0400

    fixing index

--- server/src/index.js
+++ server/src/index.js
@@ -5,13 +5,11 @@ import { userRouter } from "./routes/login.js";
 import cookieParser from "cookie-parser";
 import { topRouter } from "./routes/top.js";
 import dotenv from "dotenv";
+import { fileURLToPath } from "url"; // Import fileURLToPath
 import path from "path";
-import { fileURLToPath, URL } from "url";
 
 dotenv.config();
 const app = express();
-
-
 
 app.use(cors());
 app.use(cookieParser());
@@ -20,15 +18,21 @@ app.use(express.json());
 app.use("/auth", userRouter);
 app.use("/mytop", topRouter);
 
+// Get the current module's URL
+const __filename = fileURLToPath(import.meta.url);
+// Get the directory name from the module's URL
+const __dirname = path.dirname(__filename);
+
 // Serve static files from the React build directory
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // ... Define your API routes and other server configurations here
 
 // For any other routes, serve the index.html from the build folder
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(7001, () => console.log("Server started"));
+
 
