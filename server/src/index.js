69f52ec9d5ec209c1092a2ab1a49c5955a93d754
rawanarafat <rawanarafat@hotmail.com>
2023-08-23 18:44:39 -0400

    Index4

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,12 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-import { userRouter } from "./routes/login.js";
-import cookieParser from "cookie-parser";
-import { topRouter } from "./routes/top.js";
-import dotenv from "dotenv";
-import { fileURLToPath } from "url"; // Import fileURLToPath
-import path from "path";
+// server/index.js
+
+import express from 'express';
+import cors from 'cors';
+import { userRouter } from './routes/login.js';
+import cookieParser from 'cookie-parser';
+import { topRouter } from './routes/top.js';
+import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 const app = express();
@@ -15,18 +15,18 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
-app.use("/auth", userRouter);
-app.use("/mytop", topRouter);
+app.use('/auth', userRouter);
+app.use('/mytop', topRouter);
 
-const __dirname = dirname(new URL(import.meta.url).pathname);
+// Resolve the current working directory
+const cwd = process.cwd();
 
 // Serve static files from the React build directory
-app.use(express.static(path.resolve(__dirname, '..', 'client', 'build')));
+app.use(express.static(path.resolve(cwd, 'client', 'build')));
+
+// For any other routes, serve the index.html from the build folder
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+  res.sendFile(path.resolve(cwd, 'client', 'build', 'index.html'));
 });
 
-
-app.listen(7001, () => console.log("Server started"));
-
-
+app.listen(7001, () => console.log('Server started'));
