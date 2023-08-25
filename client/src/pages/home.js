00aa0c132b7067a091b81ef2e77db093554ba0ac
rawanarafat <rawanarafat@hotmail.com>
2023-08-24 22:53:36 -0400

    fixing callback

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -22,7 +22,7 @@ export const Home = () => {
       .then(() => {
         const accessToken = Cookies.get("access_token");
         if (accessToken) {
-          navigate("/mytop/TopTracks");
+          navigate("/mytop/Toptracks");
           //alert("login successful");
         }
       })
