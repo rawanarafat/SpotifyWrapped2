f4ed2d28236fa8a2fa6c1539f080d765530e0484
rawanarafat <rawanarafat@hotmail.com>
2023-08-24 21:56:06 -0400

    fixing callbacl

--- server/src/routes/login.js
+++ server/src/routes/login.js
@@ -60,7 +60,7 @@ router.get("/callback", async function (req, res) {
     console.log(response.data, " + ", access_token);
     //res.send("Authorization successful!");
     //res.status(200).send("Authorization successful!");
-    res.redirect("https://spotifywrapped.onrender.com/mytop/test");
+    res.redirect("https://spotifywrapped.onrender.com/mytop/TopTracks");
     // Respond to the client or redirect to another page after successful authorization
   } catch (error) {
     console.error("Error exchanging the authorization code:", error);
